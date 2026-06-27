3e5131b3e0273f8a612d93a3844b0b9e08869358
Soham Satale <sohamsatale2956@gmail.com>
2026-06-27 20:25:18 +0530

    cors update

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://atlas-erp.vercel.app", "http://localhost:5173"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
